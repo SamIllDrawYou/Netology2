2227a32827ceb5bc4728d3babe2f9a1ad10dfca7
ss.semenov <semensemenov.mmsk@gmail.com>
2025-02-17 10:38:24 +0300

    New

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Main {
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        String url = "https://api.nasa.gov/planetary/apod?api_key=lIzxjNPfySNhDZjxuCgH0XoEpa6CNu3o86BvY3uf";    //Сюда направляем запрос
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();   //Создали переменную
+        HttpGet request = new HttpGet(url); //Запрос
+        CloseableHttpResponse response = httpclient.execute(request);   //Ответ
+
+        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(),NasaAnswer.class);
+
+        String imageUrl = answer.url;
+        String[] splittedImageUrl = imageUrl.split("/");
+        String filename = splittedImageUrl[splittedImageUrl.length - 1]; //Обращаемся к последнему элементу
+
+        HttpGet imageRequest = new HttpGet(imageUrl);
+        CloseableHttpResponse image = httpclient.execute(imageRequest);
+
+        FileOutputStream fos = new FileOutputStream("Images/"+filename);
+        image.getEntity().writeTo(fos);
+
+
+    }
 }
--- src/main/java/NasaAnswer.java
+++ src/main/java/NasaAnswer.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class NasaAnswer {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NasaAnswer {
+    String copyright,
+            date,
+            explanation,
+            hdurl,
+            media_type,
+            service_version,
+            title,
+            url;
+
+    public NasaAnswer(@JsonProperty("copyright") String copyright,
+                      @JsonProperty("date")String date,
+                      @JsonProperty("explanation")String explanation,
+                      @JsonProperty("hdurl")String hdurl,
+                      @JsonProperty("media_type")String media_type,
+                      @JsonProperty("service_version")String service_version,
+                      @JsonProperty("title")String title,
+                      @JsonProperty("url")String url) {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
 }
