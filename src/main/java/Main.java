da0c6d6c6b01b9668fc6a2d96196bf721a33f176
ss.semenov <semensemenov.mmsk@gmail.com>
2025-02-17 11:15:03 +0300

    Telegram add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,25 +13,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
 
-        String url = "https://api.nasa.gov/planetary/apod?api_key=lIzxjNPfySNhDZjxuCgH0XoEpa6CNu3o86BvY3uf";    //Сюда направляем запрос
+//        String url = "https://api.nasa.gov/planetary/apod?api_key=lIzxjNPfySNhDZjxuCgH0XoEpa6CNu3o86BvY3uf";    //Сюда направляем запрос
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        CloseableHttpClient httpclient = HttpClients.createDefault();   //Создали переменную
+//        HttpGet request = new HttpGet(url); //Запрос
+//        CloseableHttpResponse response = httpclient.execute(request);   //Ответ
+//
+//        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(),NasaAnswer.class);
+//
+//        String imageUrl = answer.url;
+//        String[] splittedImageUrl = imageUrl.split("/");
+//        String filename = splittedImageUrl[splittedImageUrl.length - 1]; //Обращаемся к последнему элементу
+//
+//        HttpGet imageRequest = new HttpGet(imageUrl);
+//        CloseableHttpResponse image = httpclient.execute(imageRequest);
+//
+//        FileOutputStream fos = new FileOutputStream("Images/"+filename);
+//        image.getEntity().writeTo(fos);
 
-        ObjectMapper mapper = new ObjectMapper();
-
-        CloseableHttpClient httpclient = HttpClients.createDefault();   //Создали переменную
-        HttpGet request = new HttpGet(url); //Запрос
-        CloseableHttpResponse response = httpclient.execute(request);   //Ответ
-
-        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(),NasaAnswer.class);
-
-        String imageUrl = answer.url;
-        String[] splittedImageUrl = imageUrl.split("/");
-        String filename = splittedImageUrl[splittedImageUrl.length - 1]; //Обращаемся к последнему элементу
-
-        HttpGet imageRequest = new HttpGet(imageUrl);
-        CloseableHttpResponse image = httpclient.execute(imageRequest);
-
-        FileOutputStream fos = new FileOutputStream("Images/"+filename);
-        image.getEntity().writeTo(fos);
 
 
     }
---
+++ src/main/java/NASA_Bot.java
@@ -0,0 +1,19 @@
+
+public class NASA_Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return null;
+    }
+}
